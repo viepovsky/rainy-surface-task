fcc300122ec564e3a554d504fb97f04e004ec4de
Oskar R <o.rajzner@gmail.com>
2024-02-23 02:29:52 +0100

    Add controller tests, delete service test class

--- test/rainfall/rainfall.controller.spec.ts
+++ test/rainfall/rainfall.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RainfallController } from '../../src/rainfall/rainfall.controller';
 import { RainfallService } from '../../src/rainfall/rainfall.service';
+import { RainSurfaceDTO } from 'src/rainfall/rain.surface.dto';
 
 describe('RainfallController', () => {
   let controller: RainfallController;
@@ -19,4 +20,24 @@ describe('RainfallController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('should calculate the rain tiles of water correctly', () => {
+    const rainSurface: RainSurfaceDTO = {
+      numbers: [3, 2, 4, 1, 2]
+    }    
+    expect(controller.calculateRainTiles(rainSurface)).toEqual(2)
+
+    rainSurface.numbers = [4, 1, 1, 0, 2, 3];
+    expect(controller.calculateRainTiles(rainSurface)).toEqual(8);
+    
+    rainSurface.numbers = [1, 1, 0, 1, 3, 2, 2, 4, 1, 2, 1, 0];
+    expect(controller.calculateRainTiles(rainSurface)).toEqual(4);
+
+    rainSurface.numbers = [1, 1, 0, 1, 3, 2, 2, 4, 1, 2, 3, 0];
+    expect(controller.calculateRainTiles(rainSurface)).toEqual(6);
+
+    rainSurface.numbers = [1, 0];
+    expect(controller.calculateRainTiles(rainSurface)).toEqual(0);
+  });
+
 });
